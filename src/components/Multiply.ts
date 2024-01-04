fb6fbbfde6284d6638a8d33a32e2812187e82c57
bharatgareja <garejabharatk@gmail.com>
2024-01-04 11:15:12 +0530

    add multiply function

---
+++ src/components/Multiply.ts
@@ -0,0 +1,11 @@
+/**
+ * Takes two numbers and returns their sum
+ * 
+ * @param [a] - first input to multiply
+ * @param [b] - second input to multiply
+ * @returns multiply of a and b
+ */
+
+export default function Multiply(a:number,b:number){
+    return a * b;
+}
