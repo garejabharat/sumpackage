358aa43ac3bb015220cd9f09c1442f755cd6fc23
bharatgareja <garejabharatk@gmail.com>
2024-01-05 12:58:12 +0530

    add comment on buildspec file

---
+++ dist/components/Multiply.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Takes two numbers and returns their sum
+ *
+ * @param [a] - first input to multiply
+ * @param [b] - second input to multiply
+ * @returns multiply of frist and second parameter
+ */
+export default function Multiply(a: number, b: number): number;
---
+++ dist/components/Multiply.js
@@ -0,0 +1,13 @@
+"use strict";
+/**
+ * Takes two numbers and returns their sum
+ *
+ * @param [a] - first input to multiply
+ * @param [b] - second input to multiply
+ * @returns multiply of frist and second parameter
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+function Multiply(a, b) {
+    return a * b;
+}
+exports.default = Multiply;
